c53a6f0e6abc903acc866e4b236d9b59d93318db
Elhanzar <elhanzaromurzakov23@gmail.com>
2024-04-01 12:33:33 +0500

    Добавил взаимодействие с сервером

--- Bzbzbz/Bzbzbz/MainWindow.xaml.cs
+++ Bzbzbz/Bzbzbz/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Bzbzbz
 {
@@ -19,6 +22,95 @@ namespace Bzbzbz
         public MainWindow()
         {
             InitializeComponent();
+        }
+        StreamReader? Reader = null;
+        StreamWriter? Writer = null;
+        ~MainWindow()
+        {
+            Writer?.Close();
+            Reader?.Close();
+        }
+        TcpClient client = new TcpClient();
+        private void Connection_Click(object sender, RoutedEventArgs e)
+        {
+            string host = Host.Text;
+            int port = 8888;
+            string? userName = Name.Text;
+
+            try
+            {
+                client.ConnectAsync(host, port); //подключение клиента
+                Reader = new StreamReader(client.GetStream());
+                Writer = new StreamWriter(client.GetStream());
+                if (Writer is null || Reader is null) return;
+                // запускаем новый поток для получения данных
+                Task.Run(() => ReceiveMessageAsync(Reader));
+                //welcom
+                MassegeGenerate($"Добро пожаловать, {userName}", HorizontalAlignment.Center);
+                // сначала отправляем имя
+                Writer.WriteLineAsync(Name.Text.ToString());
+                Writer.FlushAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            // запускаем ввод сообщений
+            SendMessageAsync();
+            MassegeGenerate(Message.Text, HorizontalAlignment.Right);
+            Message.Text = default;
+        }
+        // отправка сообщений
+        async Task SendMessageAsync()
+        {
+            if (Writer is null || Reader is null) return;
+            try
+            {
+                await Writer.WriteLineAsync(Message.Text.ToString());
+                await Writer.FlushAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        void MassegeGenerate(string massage, HorizontalAlignment horizontalAlignment)
+        {
+            Border border = new Border();
+            TextBlock textBlock = new TextBlock();
+            textBlock.TextWrapping = TextWrapping.Wrap;
+            textBlock.Text = massage;
+            textBlock.Foreground = Brushes.White;
+            border.CornerRadius = new CornerRadius(8);
+            border.Padding = new Thickness(5);
+            border.Child = textBlock;
+            border.Margin = new Thickness(5);
+            border.BorderThickness = new Thickness(1);
+            border.BorderBrush = Brushes.White;
+            border.HorizontalAlignment = horizontalAlignment;
+            border.VerticalAlignment = VerticalAlignment.Top;
+            Chat.Children.Add(border);
+        }
+        // получение сообщений
+        async Task ReceiveMessageAsync(StreamReader reader)
+        {
+            while (true)
+            {
+                try
+                {
+                    // считываем ответ в виде строки
+                    string? message = await reader.ReadLineAsync();
+                    if (string.IsNullOrEmpty(message)) continue;
+                    Dispatcher.Invoke(() => MassegeGenerate(message, HorizontalAlignment.Left));
+                }
+                catch
+                {
+                }
+            }
         }
     }
 }
